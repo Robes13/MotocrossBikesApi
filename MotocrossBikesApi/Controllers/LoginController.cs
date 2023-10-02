37a7fa997d242d8f6a993b9b3f2df28e1443be4f
Mathias Jensen <mathiasbeckj@gmail.com>
2023-10-02 13:21:40 +0200

    Added controllers and tokenservice

---
+++ MotocrossBikesApi/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MotocrossBikesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Login()
+        {
+
+        }
+    }
+}
---
+++ MotocrossBikesApi/Services/TokenService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MotocrossBikesApi.Services
+{
+    public class TokenService
+    {
+        private readonly IConfiguration _configuration;
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// Generates a jwt token, with claims from the user provided.
+        /// </summary>
+        /// <param name="user">User</param>
+        /// <returns>A valid jwt token, or null if an error occurs</returns>
+        public string GenerateJwtToken(User user)
+        {
+            try
+            {
+                // Create key
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:SecretKey"]));
+
+                var tokenExpirationInHours = 1;
+
+                // Create claims
+                var claims = new List<Claim>
+                {
+                //new Claim(ClaimTypes.Name, user.Name),
+                //new Claim(ClaimTypes.Role, user.Role.ToLower())
+                };
+                // Create token
+                var token = new JwtSecurityToken(null, null, claims, null, DateTime.Now.AddHours(tokenExpirationInHours), new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature));
+
+                // Serialize token
+                string tokenvalue = new JwtSecurityTokenHandler().WriteToken(token);
+
+                return tokenvalue;
+            }
+            catch (Exception ex)
+            {
+                File.AppendAllTextAsync("error.log", "\n" + ex.Message);
+
+                return null;
+            }
+        }
+    }
+}
